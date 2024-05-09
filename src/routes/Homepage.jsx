b43a22872ce797750638928d27f3ea8330f95402
NStav <nstavropoulos@deakin.edu.au>
2024-05-09 16:05:52 +1000

    fixed repo

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -4,7 +4,7 @@ import Scale from "../Scale";
 import Screen from "../Screen";
 import PowerButton from '../PowerButton';
 import ScaleImg from "../img/Scale(Figma).png"
-//test
+//test fr
 function Homepage() {
 
     var [status, setStatus] = React.useState('none')
